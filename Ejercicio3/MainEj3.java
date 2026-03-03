b01527b3ade6cb902d117a074b57a8cc2d03d2da
MaguiLuque <luquemag.23@gmail.com>
2026-03-03 02:50:01 -0400

    subiendo ejercicio 3

---
+++ Ejercicio3/MainEj3.java
@@ -0,0 +1,14 @@
+package Ejercicio3;
+/**
+ * @author Magdalena
+ */
+public class MainEj3 {
+    public static void main(String[] args) {
+        Instrumento i1 = new Instrumento("gitarra", "madera", "cuerda");
+        Instrumento i2 = new Instrumento("quena", "plastico", "viento");
+        
+        System.out.println(i1);
+        System.out.println(i2);
+    }
+    
+}
